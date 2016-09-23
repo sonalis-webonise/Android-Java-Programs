1b014dc6124730ea66cee27a3d999c1226cf6070
sonalis-webonise <sssonali63@gmail.com>
2016-09-23 15:31:24 +0530

    Changes made in Area

---
+++ Area/src/Shape.java
@@ -0,0 +1,6 @@
+/**
+ * Created by webonise on 22/9/16.
+ */
+public abstract class Shape {
+    abstract double calculateArea(double height,double width);
+}
---
+++ Area/src/Triangle.java
@@ -0,0 +1,10 @@
+/**
+ * Created by webonise on 14/9/16.
+ */
+public class Triangle extends Shape {
+
+    @Override
+    double calculateArea(double height, double width) {
+        return (height*width)/2;
+    }
+}
